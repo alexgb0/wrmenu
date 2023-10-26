eb244b068d1b1b3e6725d092d4744a32befc57a1
alexgb0 <alexgb.git@gmail.com>
2023-10-26 23:29:59 +0200

    Added list of files and you can select items

--- src/main.c
+++ src/main.c
@@ -10,8 +10,8 @@ int main()
 
 	struct files_list files = list_files();
 
-	static int screen_w = 800;
-	static int screen_h = 100;
+	static int screen_w = 500;
+	static int screen_h = 500;
 
 	SetTraceLogLevel(LOG_ERROR);
 	InitWindow(screen_w, screen_h, "wrmenu");
@@ -24,15 +24,33 @@ int main()
 		.input_buff_len = 0,
 	};
 
+	const int font_heigh = 15;
+	int selected_program = 0;
 
 	while (!WindowShouldClose())
 	{
 		draw_searchbox(&buff_t);
-		
+
+		if (IsKeyPressed(KEY_DOWN) &&
+				selected_program < files.filenm_len - 1)
+		{
+			selected_program++;
+		}
+		if (IsKeyPressed(KEY_UP) && selected_program > 0)
+		{
+			selected_program--;
+		}
 
 		BeginDrawing();
 		ClearBackground(BLACK);
+
 		DrawText(buff_t.buff, 5, 5, 15, WHITE);
+		for (int i = 0; i < files.filenm_len - 1; ++i)
+		{
+			DrawText(files.filenm[i], 5, (font_heigh + 5) * (i + 1), 15, 
+					(i == selected_program) ? PINK : GRAY);
+		}
+
 		EndDrawing();
 	}
 
